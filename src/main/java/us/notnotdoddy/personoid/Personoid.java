34ea9bbdfe247e9d8751af5dd5c64d1728d9e13d
notnotnotswipez <strubbe06@gmail.com>
2021-08-24 16:37:47 -0400

    Initial commit

---
+++ src/main/java/us/notnotdoddy/personoid/Personoid.java
@@ -0,0 +1,17 @@
+package us.notnotdoddy.personoid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Personoid extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
