049071ab0851977b5d05f5c9db7e608a1fff8832
DefineDoddy <brandonharleydodd@gmail.com>
2021-08-26 20:17:08 +0100

    Added/tweaked more intents and added levenshtein distance support for finding responses

--- src/main/java/us/notnotdoddy/personoid/ChatMessage.java
+++ src/main/java/us/notnotdoddy/personoid/ChatMessage.java
@@ -10,9 +10,11 @@ import me.definedoddy.fluidapi.FluidPlugin;
 import me.definedoddy.fluidapi.FluidUtils;
 import me.definedoddy.fluidapi.tasks.DelayedTask;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 
@@ -43,19 +45,31 @@ public class ChatMessage {
         new FluidListener<>(AsyncPlayerChatEvent.class) {
             @Override
             public void run() {
-                if (FluidUtils.random(1, 10) >= 0) { //3
-                    NPC npc = NPCHandler.getNPCs().values().stream().toList().get(0);
+                NPC npc = null;
+                for (NPC potential : NPCHandler.getNPCs().values()) {
+                    if (getData().getMessage().toLowerCase().contains(potential.entity.getName().toLowerCase())) {
+                        npc = potential;
+                    }
+                }
+                if (npc == null) {
+                    npc = NPCHandler.getNPCs().values().stream().toList().get(FluidUtils.random(0, NPCHandler.getNPCs().size() - 1));
+                }
+                if (FluidUtils.random(1, 10) >= 3) {
+                    NPC finalNpc = npc;
                     new DelayedTask(FluidUtils.random(10, 60)) {
                         @Override
                         public void run() {
-                            npc.pause();
-                            String response = getResponse(getData().getPlayer(), getData().getMessage(), npc);
-                            int delay = Math.min(Math.max(response.length() * 5 + FluidUtils.random(0, 10), 5), 100);
+                            finalNpc.pause();
+                            String response = getResponse(getData().getPlayer(), getData().getMessage(), finalNpc);
+                            int delay = response == null ? FluidUtils.random(0, 10) :
+                                    Math.min(Math.max(response.length() * 5 + FluidUtils.random(0, 10), 5), 100);
                             new DelayedTask(delay) {
                                 @Override
                                 public void run() {
-                                    new ChatMessage(response, npc);
-                                    npc.resume();
+                                    if (response != null) {
+                                        new ChatMessage(response, finalNpc);
+                                    }
+                                    finalNpc.resume();
                                 }
                             };
                         }
@@ -68,18 +82,37 @@ public class ChatMessage {
     private static String getResponse(Player player, String message, NPC npc) {
         Behavior.Mood mood = npc.players.get(player).mood;
         JsonArray array = (JsonArray) intents.get("intents");
+        JsonObject closestIntent = null;
+        int closestDistance = Integer.MAX_VALUE;
         for (JsonElement intent : array) {
             JsonArray patterns = ((JsonObject)intent).get("patterns").getAsJsonArray();
-            if (patterns.getAsJsonArray().toString().contains(message)) {
-                JsonObject responses = (JsonObject)((JsonObject) intent).get("responses");
-                JsonElement element = responses.get(mood.name().toLowerCase());
-                String response = ((JsonArray)element).get(FluidUtils.random(0, responses.size() - 1)).toString();
-                return response.substring(1, response.length() - 1);
+            for (JsonElement pattern : patterns) {
+                String string = pattern.toString();
+                string = string.substring(1, string.length() - 1);
+                int distance = StringUtils.getLevenshteinDistance(message, string);
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestIntent = intent.getAsJsonObject();
+                }
             }
         }
+        if (closestDistance < 15) {
+            JsonObject responses = (JsonObject)(closestIntent).get("responses");
+            return getRandomFromMood(mood, responses);
+        }
         JsonObject responses = (JsonObject) intents.get("backup");
-        JsonElement element = responses.get(mood.name().toLowerCase());
-        String response = ((JsonArray)element).get(FluidUtils.random(0, responses.size() - 1)).toString();
-        return response.substring(1, response.length() - 1);
+        return getRandomFromMood(mood, responses);
+    }
+
+    @Nullable
+    private static String getRandomFromMood(Behavior.Mood mood, JsonObject responses) {
+        JsonElement list = responses.get(mood.name().toLowerCase());
+        JsonElement element = ((JsonArray)list).get(FluidUtils.random(0, responses.size() - 1));
+        if (element != null) {
+            String response = element.toString();
+            return response.substring(1, response.length() - 1);
+        } else {
+            return null;
+        }
     }
 }
